63411de93558da38397be768c95b6fb0bda6daa2
Patricio <42851366+patohh@users.noreply.github.com>
2018-10-31 14:23:24 +0100

    primer cuerto commit

---
+++ js/hombres/animaciones.js
@@ -0,0 +1,14 @@
+
+    /*Abrir y cerrar menu lateral*/ 
+    function abrir(){
+
+        document.getElementById('ul').style.width = "60%";
+    }
+
+    function cerrar(){
+        
+        document.getElementById('ul').style.width= "0%";
+    }
+
+
+  
